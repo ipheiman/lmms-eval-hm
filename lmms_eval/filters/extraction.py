365f25a9bf3e96d7cfa7f3e8d4d220fddadc1da8
Ip Hei Man <iheiman@dso.org.sg>
2025-08-07 16:02:32 +0800

    Added Last Puntuation Filter

--- lmms_eval/filters/extraction.py
+++ lmms_eval/filters/extraction.py
@@ -29,18 +29,19 @@ class WhitespaceFilter(Filter):
 
         return filtered_resps
 
-class PunctuationFilter(Filter):
+class LastPunctuationFilter(Filter):
     """Filter to remove all punctuation characters from responses."""
 
     def __init__(self) -> None:
-        self.translator = str.maketrans("", "", string.punctuation)
+       pass
 
     def apply(self, resps, docs):
         def filter_set(inst):
             filtered_resp = []
             for resp in inst:
-                # Remove punctuation using the translation table
-                resp = resp.translate(self.translator)
+                # Remove last punctuation
+                if resp.endswith("."):
+                    resp = resp[:-1]
                 filtered_resp.append(resp)
             return filtered_resp
 
